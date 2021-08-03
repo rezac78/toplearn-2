5505027de814d10de831f4453314ac13b036cd20
reza <69678342+rezac78@users.noreply.github.com>
2021-08-03 00:49:04 -0700

    oky

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -2,5 +2,5 @@ import http from "./httpService";
 import config from "./config.json";
 
 export const getCourses = () => {
-    return http.get(`${config.toplearnapi}/api/courses`);
-};
+    return http.get(`${config.toplearnapi}/api/courses`)
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Reducers } from './../reducers/index';
+import thunk from 'redux-thunk';
+import { getAllCourses } from './../actions/courses';
 
-const store=createStore(Reducers)
+const store = createStore(Reducers, compose(applyMiddleware(thunk)))
+
+store.dispatch(getAllCourses());
+
+
+store.subscribe(() => console.log(store.getState()))
